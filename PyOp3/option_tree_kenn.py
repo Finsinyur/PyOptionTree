e99e1ef339a98907db2b58ada5a797b68f12c3a7
kennong <119650383+kennong@users.noreply.github.com>
2022-12-21 17:39:21 +0800

    Create option_tree_kenn.py
    This is a draft that returns the option price. Supposed to build on the class that Caden has created but i have a syntax error that i can't solve at line 18. =\

---
+++ PyOp3/option_tree_kenn.py
@@ -0,0 +1,36 @@
+from binomial_tree import fit_tree
+import numpy as np
+import base_conditions
+import pandas as pd
+
+def option_tree(S0,r,T,N,K,u,opt='european',call=True,cont_disc=False):
+    #determine d
+    self.d=1/self.u
+    
+    #initialise the asset and option matrix
+    V=np.zeros([self.step+1,self.step+1])
+    S=np.zeros((self.step+1,self.step+1))
+    
+    #discount factor
+    disc = np.exp(-self.r*T) if cont_disc==True else 1/((1+self.r)**(self.step) #to confirm the formula
+    
+    #find ST first
+    S[:,-1] = self.underlying_asset.spot_price_price*self.d**(np.arange(self.step,-1,-1))*self.u**(np.arange(0,self.step+1,1))                                           
+    
+    #set the terminal price
+    V[:,-1] = np.maximum(0, disc*(self.spot_price-K)) if call == True else np.maximum(0, disc*(K-self.spot_price))
+    
+    #risk-neutral probability
+    p=(1/disc-self.d)/(self.u-self.d) if cont_disc=True else (1+r-d)/(u-d)
+    
+    for i in np.arange(N-1,-1,-1):
+        V[:i+1,i]= disc*(V[1:i+2,i+1]*p+V[:i+1,i+1]*(1-p))
+        
+        if opt!='european':
+    # Recall: if option is American, the value of option at t is the bigger of the discounted value at t+1 or the difference between spot price and strike.
+            if call==True:
+                V[:i+1,i]= np.maximum(V[:i+1,i],S[:i+1,i]-K)
+            else:
+                V[:i+1,i]= np.maximum(V[:i+1,i],K-S[:i+1,i])
+    
+    return V
