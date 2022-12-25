2400878b01008c74062957a26c414f0c5f6603df
Caden.Finsinyur <75693234+Finsinyur@users.noreply.github.com>
2022-12-25 11:09:07 +0800

    Added comments on the codes.

--- PyOp3/option_tree_kenn.py
+++ PyOp3/option_tree_kenn.py
@@ -9,23 +9,27 @@ def option_tree(S0,r,T,N,K,u,opt='european',call=True,cont_disc=False):
     
     #initialise the asset and option matrix
     V=np.zeros([self.step+1,self.step+1])
-    S=np.zeros((self.step+1,self.step+1))
+    S=np.zeros((self.step+1,self.step+1)) # Caden: We already created a class that can generate underlying asset lattice, we can just make use of that
     
     #discount factor
-    disc = np.exp(-self.r*T) if cont_disc==True else 1/((1+self.r)**(self.step) #to confirm the formula
+    disc = np.exp(-self.r*T) if cont_disc==True else 1/((1+self.r)**(self.step) #to confirm the formula 
+    # Caden: formula is not correct. for continuous discounting: np.exp(-self.r*delta_T); for non-cont., should be 1/(1+self.r*self.delta_t)
     
     #find ST first
     S[:,-1] = self.underlying_asset.spot_price_price*self.d**(np.arange(self.step,-1,-1))*self.u**(np.arange(0,self.step+1,1))                                           
-    
+    # Redundant; could make use of underlying_asset_tree
+                                                        
     #set the terminal price
     V[:,-1] = np.maximum(0, disc*(self.spot_price-K)) if call == True else np.maximum(0, disc*(K-self.spot_price))
+    # At terminal node, should be strike - terminal spot price, which should be the last col of the underlying asset tree
     
     #risk-neutral probability
-    p=(1/disc-self.d)/(self.u-self.d) if cont_disc=True else (1+r-d)/(u-d)
+    p=(1/disc-self.d)/(self.u-self.d) if cont_disc=True else (1+r-d)/(u-d) # should be cont_disc == True
     
     for i in np.arange(N-1,-1,-1):
         V[:i+1,i]= disc*(V[1:i+2,i+1]*p+V[:i+1,i+1]*(1-p))
-        
+    
+    # We can split European and American; 
         if opt!='european':
     # Recall: if option is American, the value of option at t is the bigger of the discounted value at t+1 or the difference between spot price and strike.
             if call==True:
