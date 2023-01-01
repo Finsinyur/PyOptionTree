7e52e90556baa58d652fa683d42efdb7343896d4
Caden.Finsinyur <75693234+Finsinyur@users.noreply.github.com>
2023-01-02 02:33:04 +0800

    Update __init__.py

--- pyop3/__init__.py
+++ pyop3/__init__.py
@@ -5,17 +5,35 @@ from .base_conditions import (
 
 from .binomial_tree import(
     binomial_tree,
-    european_option
+    european_option,
+    american_option
     )
 
 from .tree_planter import(
     show_tree
     )
 
-__version__ = "0.0.1"
+from .tools import(
+    get_trading_days
+    )
+
+from .calibration import(
+    calibrate_european,
+    calibrate_american,
+    deamericanization
+    )
+
+__version__ = "0.0.2"
 
 __all__ = [
     "base_asset",
     "base_rate",
-    "fit_asset"
+    "binomial_tree",
+    "european_option",
+    "american_option",
+    "show_tree",
+    "get_trading_days",
+    "calibrate_european",
+    "calibrate_american",
+    "deamericanization"
 ]
